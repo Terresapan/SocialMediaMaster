63c481fd92b69a18f20b46c69945ed899adff10a
Terresapan <terresap2010@gmail.com>
2024-10-07 23:47:32 +0000

    major update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,127 @@
 import streamlit as st
-from openai import OpenAI
+import time
+from utils import save_feedback
+import google.generativeai as genai
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
+# Enhanced Streamlit chatbot interface
+st.sidebar.header("✨ Short Video Content Master")
+st.sidebar.markdown(
+    "This app answer your questions about Short Video Content Strategy and Execution. "
+    "To use this App, you need to provide a Google Gemini API key, which you can get [here](https://aistudio.google.com/app/apikey) for free.")
+st.sidebar.write("### Instructions")
+st.sidebar.write(":pencil: Enter your video topic, target audience, theme, and question you want to ask.")
+st.sidebar.write(":point_right: Click 'Generate Suggestions' to receive suggestions.")
+st.sidebar.write(":point_right: Click 'Click me if you have no idea what to ask' to get inspired.")
+st.sidebar.write(":heart_decoration: Tell me your thoughts and feedback about the App.")
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+# Initialize session state for feedback storage if not already done
+if 'feedback' not in st.session_state:
+    st.session_state.feedback = ""
+
+# Feedback Form
+st.sidebar.subheader("Feedback Form")
+feedback = st.sidebar.text_area("Your Thoughts and Feedback", value=st.session_state.feedback, placeholder="Share your feedback here...")
+    
+if st.sidebar.button("Submit Feedback"):
+    if feedback:
+        try:
+            save_feedback(feedback)
+            st.session_state.feedback = ""  # Clear feedback after submission
+            st.sidebar.success("Thank you for your feedback! 😊")
+        except Exception as e:
+            st.sidebar.error(f"Error saving feedback: {str(e)}")
+    else:
+        st.sidebar.error("Please enter your feedback before submitting.")
+
+st.sidebar.image("assets/logo01.jpg", use_column_width=True)
+
+# ask user for their Gemini API key via `st.text_input`.
+gemini_api_key = st.text_input("Gemini API Key", type="password", placeholder="Your Gemini API Key here...")
+
+if not gemini_api_key:
+    st.info("Please add your Gemini API key to continue.", icon="🗝️")
 else:
+    # create an openai client
+    genai.configure(api_key=gemini_api_key)
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    def upload_to_gemini(path, mime_type=None):
+       """Uploads the given file to Gemini."""
+       file = genai.upload_file(path, mime_type=mime_type)
+       print(f"Uploaded file '{file.display_name}' as: {file.uri}")
+       return file
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+    def wait_for_files_active(files):
+        """Waits for the given files to be active."""
+        print("Waiting for file processing...")
+        for name in (file.name for file in files):
+            file = genai.get_file(name)
+            while file.state.name == "PROCESSING":
+                print(".", end="", flush=True)
+                time.sleep(10)
+                file = genai.get_file(name)
+            if file.state.name != "ACTIVE":
+                raise Exception(f"File {file.name} failed to process")
+            print(file)
+        print("...all files ready")
+   
+    # Make these files available on the local file system
+    # You may need to update the file paths
+    files = [
+        upload_to_gemini("assets/Social Media Blogs.pdf", mime_type="application/pdf"),
+        upload_to_gemini("assets/Social Media Video Marketing for small business owers.pdf", mime_type="application/pdf")
+    ]
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+    # Some files have a processing delay. Wait for them to be ready.
+    wait_for_files_active(files)
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
 
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+    # input field for user to input a product description
+    st.header("Enter Your Video Details")
+    video_topic_input = st.text_input("Enter a Video topic", placeholder="organic soup")
+    target_audience_input = st.text_input("Enter the Target Audience", placeholder="for busy working adults")
+    selling_point_input = st.text_input("Enter Unique Selling Points or Opinion", placeholder="ready in 5 minutes")
+    question_input = st.text_input("Enter Your Question", placeholder="How can I make the video hook?")
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
+    system_message = f"""
+        You are a highly skilled expert in social media strategies, specializing in the creation of engaging short video content to drive lead generation.
+        You will be provided the basic info about the user, including {video_topic_input},{target_audience_input},{selling_point_input}.
+        Your role is to anwser {question_input} and assist users in producing effective short videos based on a series of provided PDFs, which serve as your primary source of information.
+        Adhere strictly to the content in these PDFs. If the information requested is nothing to do with the provided materials,
+        clearly state that you do not have the answer, rather than providing speculative responses.
+        If users raise questions unrelated to content creation, politely remind them to focus on the topic at hand.
+    """
 
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+    # button to trigger tagline generation
+    if st.button("Generate Suggestions"):
+        # Validate inputs
+        if not video_topic_input or not target_audience_input or not selling_point_input or not question_input:
+            st.error("Please fill out all fields to generate suggestions.", icon="🚫")
+        else:
+            generation_config = {
+                "temperature": 0,
+                "top_p": 0.95,
+                "top_k": 64,
+                "max_output_tokens": 8192,
+                "response_mime_type": "text/plain",
+                }
+            
+            model = genai.GenerativeModel(
+                model="gemini-1.5-flash", 
+                generation_config=generation_config,
+                system_instruction=system_message,
+            )
+    
+            chat_session = model.start_chat(
+                history=[
+                    {
+                        "role": "user",
+                        "parts": [
+                        *files,
+                        ],
+                    },
+                    ]
+                )
+            response = chat_session.send_message(question_input)
+                # Display messages
+            st.subheader("Generated Suggestions")
+            st.write(f"🤖 AI: {response.text}")
---
+++ utils.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import os
+import time
+import google.generativeai as genai
+
+# Function to save feedback to a file
+def save_feedback(feedback_text):
+    feedback_dir = "feedback"
+    feedback_file_path = os.path.join(feedback_dir, "feedback.txt")
+
+    # Ensure feedback directory exists
+    os.makedirs(feedback_dir, exist_ok=True)
+
+    # Save feedback to the file
+    try:
+        with open(feedback_file_path, "a") as file:
+            file.write(feedback_text + "\n")
+    except Exception as e:
+        raise IOError(f"Error saving feedback: {str(e)}")
+    
